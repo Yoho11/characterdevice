25309be9e3c74efcfe96a4b82e4380bbf7c4aec1
Yoho11 <43831058+Yoho11@users.noreply.github.com>
2020-03-06 00:47:23 +0100

    Update hello.c
    Fixato il bug per cui la lettura restituiva un solo carattere, e altri bug di minore importanza.

--- hello.c
+++ hello.c
@@ -19,6 +19,7 @@ void hello_exit(void);
 int hello_init(void);
 /* Struttura file operations*/
 struct file_operations memory_fops = {  
+	.owner=THIS_MODULE,
 	.read= h_read,  
 	.write= h_write,  
 	.open=h_open,  
@@ -29,9 +30,13 @@ module_init(hello_init);
 module_exit(hello_exit);
 /* Major number */
 int major = 241;
-/* Buffer dati */
-char *memory_buffer;
+/* variabili ausiliarie */
+int ret;
+int left_copy;
 static short size_of_message;
+/*buffer di memoria*/
+char *memory_buffer;
+
 int hello_init(void){
 	int result;
 	/* Registrazione device*/
@@ -41,10 +46,10 @@ int hello_init(void){
 	return result;
 	}
 	/*Allocazione memoria*/
-	memory_buffer=kmalloc(1,GFP_KERNEL);
+	memory_buffer=kmalloc(10,GFP_KERNEL);
 	if(!memory_buffer){
 		result=-ENOMEM;
-		goto fail;
+			goto fail;
 	}
 	memset(memory_buffer,0,1);
 	printk("<1>Inserimento modulo\n");
@@ -74,21 +79,42 @@ int h_release(struct inode *inode, struct file *flip){
 	return 0;
 }
 
+/*
+Metodo chiamato quando l'userspace legge dal dispositivo
+copy_to_user passa i dati all'userspace specificato in @buf, restituisce 0 se ha finito di trasmettere
+*/
 ssize_t h_read(struct file *flip, char *buf, size_t count, loff_t *f_pos){
 	/* Trasferimento dati */
-	copy_to_user(buf, memory_buffer,1);
-	if(*f_pos==0){
-		*f_pos+=1;
-		return 1;
-	}else{
+	size_t remain;
+	ret=0;
+	//memory_buffer="dota2";
+	remain=strlen(memory_buffer);	
+	left_copy=(remain<=count)?
+			remain:count;	
+	ret=copy_to_user(buf, memory_buffer,left_copy);
+	*f_pos+=remain-ret;
+	if(*f_pos>remain){
 		return 0;
+	}else{
+		return remain;
 	}
 }
-
+/*
+Metodo chiamato per scrivere nel character device
+La memoria viene riallocata ad ogni chiamata per evitare segmentation fault
+*/
 ssize_t h_write(struct file *flip,const char *buf, size_t count, loff_t *f_pos){
-	char *tmp;
-	tmp=buf+count-1;
-	copy_from_user(memory_buffer, tmp, 1);
-	return 1;
+	//Rilascio memoria 
+	if(memory_buffer){
+		kfree(memory_buffer);
+	}
+	//Allocazione memoria
+	memory_buffer=kmalloc(count,GFP_KERNEL);
+	if(!memory_buffer){
+		hello_exit();
+		return -ENOMEM;
+	}	
+	size_of_message=strlen(buf);
+	copy_from_user(memory_buffer, buf, count);
+	return count;
 }
-
